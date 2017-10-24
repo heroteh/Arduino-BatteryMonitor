b40ba6df682bca4972fee10e1a260a6e499d0f17
Tony Hills <heroteh@hotmail.com>
2017-10-24 09:15:55 -0400

    Basic display working.

--- BatteryMonitor/BatteryMonitor.ino
+++ BatteryMonitor/BatteryMonitor.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_SSD1306.h>
 
-Adafruit_SSD1306 display;
+Adafruit_SSD1306 *display;
+#define OLED_RESET -1
 
 void setup() 
 {
@@ -8,10 +9,20 @@ void setup()
   while(!Serial); 
   Serial.println(" ");
   Serial.println("Starting up...");
+
+  display = new Adafruit_SSD1306(OLED_RESET);
+  display->begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
 }
 
 void loop() 
 {
   Serial.println("Running");
+  display->ssd1306_command(SSD1306_DISPLAYON);
+  display->clearDisplay();
+  display->setTextSize(2);
+  display->setTextColor(WHITE);
+  display->setCursor(0,0);
+  display->println("Hello world!");
+  display->display();
   delay(30 * 1000);
 }
